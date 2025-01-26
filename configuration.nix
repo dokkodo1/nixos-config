be4caf73d164ca25afe9ff9f76178942fae09c5e
dokkodo1 <callum.dokkodo@gmail.com>
2025-01-26 17:41:33 +0200

    Update NixOS configuration

--- configuration.nix
+++ configuration.nix
@@ -32,10 +32,16 @@
       ### STAR CITIZEN STUFF
       substituters = ["https://nix-citizen.cachix.org"];
       trusted-public-keys = ["nix-citizen.cachix.org-1:lPMkWc2X8XD4/7YPEEwXKKBg+SVbYTVrAaLA2wQTKCo="];
+      ### ^^^^^
     };
   };
-  
-  
+  ### STAR CITIZEN STUFF
+  boot.kernel.sysctl = {
+    "vm.max_map_count" = 16777216;
+    "fs.file-max" = 524288;
+  };  
+  ### ^^^^^
+
   security.rtkit.enable = true; # For real-time tasks 
 
 
