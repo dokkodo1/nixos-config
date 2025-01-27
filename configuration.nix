b0f439d3bd98525975e070a0e6076c493fcc0014
dokkodo1 <callum.dokkodo@gmail.com>
2025-01-27 21:02:19 +0200

    Update NixOS configuration

--- configuration.nix
+++ configuration.nix
@@ -18,6 +18,24 @@
       lactd.wantedBy = ["multi-user.target"];
       systemd-journal-upload.enable = true;
     };
+    #dropbox
+    user.services.dropbox = {
+      description = "Dropbox";
+      wantedBy = [ "graphical-session.target" ];
+      environment = {
+        QT_PLUGIN_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtPluginPrefix;
+        QML2_IMPORT_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtQmlPrefix;
+      };
+      serviceConfig = {
+        ExecStart = "${lib.getBin pkgs.dropbox}/bin/dropbox";
+        ExecReload = "${lib.getBin pkgs.coreutils}/bin/kill -HUP $MAINPID";
+        KillMode = "control-group"; # upstream recommends process
+        Restart = "on-failure";
+        PrivateTmp = true;
+        ProtectSystem = "full";
+        Nice = 10;
+      };      
+    };
   };
   
   nixpkgs.config = {
@@ -51,6 +69,10 @@
   networking = {
     hostName = "nixos";
     networkmanager.enable = true;
+
+    #dropbox
+    allowedTCPPorts = [ 17500 ];
+    allowedUDPPorts = [ 17500 ];
   };
 
 
