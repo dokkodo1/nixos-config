cf9fe9663e610b53d3462639b094abbb3873821b
dokkodo <callum.dokkodo@gmail.com>
2026-07-14 00:38:41 +0200

    latitude haskell

--- hosts/latitude/configuration.nix
+++ hosts/latitude/configuration.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, hostVars, ... }:
 
 {
   imports = [
     ./hardware-configuration.nix
   ];
+
   nixpkgs.config.permittedInsecurePackages = [ "electron-39.8.10" ]; # failed build, probably temporary fix. didn't feel like searching for which package broke it...
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -26,9 +27,22 @@
   programs.nix-ld.enable = true;
   programs.firefox.enable = true;
   services.displayManager.sddm.enable = false;
+  home-manager.users.${hostVars.username} = {
+    # services.emacs.enable = true;
+    # programs.emacs = {
+    #   enable = true;
+    #   extraConfig = ''
+    #     source-file ${./init.el}
+    #   '';
+    # };
+  };
 
   environment.systemPackages = with pkgs; [
+    zathura
+    ghc
+    python3
     vlc
+    reaper
     deluge
     teamspeak6-client
     bitwarden-desktop
